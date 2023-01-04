fc2a299f1f2547013b32f86f736cf36f63a7feba
Valentin Jebelev <vala.jebelev@gmail.com>
2023-01-04 14:14:16 +0200

    Add location name constants

--- src/LocationManager.php
+++ src/LocationManager.php
@@ -13,11 +13,19 @@ use function sprintf;
 
 class LocationManager extends ParentDelegator
 {
-    public const POST_TYPE     = 'post_type';
-    public const TAXONOMY      = 'taxonomy';
-    public const PAGE_TEMPLATE = 'page_template';
-    public const USER_FORM     = 'user_form';
-    public const POST_STATUS   = 'post_status';
+    public const POST_TYPE         = 'post_type';
+    public const TAXONOMY          = 'taxonomy';
+    public const PAGE_TEMPLATE     = 'page_template';
+    public const USER_FORM         = 'user_form';
+    public const POST_STATUS       = 'post_status';
+    public const CURRENT_USER      = 'current_user';
+    public const CURRENT_USER_ROLE = 'current_user_role';
+    public const NAV_MENU          = 'nav_menu';
+    public const PAGE_TYPE         = 'page_type';
+    public const POST_TAXONOMY     = 'post_taxonomy';
+    public const POST_CATEGORY     = 'post_category';
+    public const BLOCK             = 'block';
+    public const OPTIONS_PAGE      = 'options_page';
 
     /** @var array<array{param: string, operator: string, value: mixed}> */
     private array $locations;
