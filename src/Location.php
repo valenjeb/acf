e5f1b95be7f84b2c1fa46d1fd0c05a2ce0e1cd98
Valentin Jebelev <vala.jebelev@gmail.com>
2023-12-07 07:17:26 +0200

    Add location enum

---
+++ src/Location.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Devly\ACF;
+
+class Location
+{
+    public const POST_TYPE = 'post_type';
+    public const TAXONOMY = 'taxonomy';
+    public const PAGE_TEMPLATE = 'page_template';
+    public const USER_FORM = 'user_form';
+    public const POST_STATUS = 'post_status';
+    public const CURRENT_USER = 'current_user';
+    public const CURRENT_USER_ROLE = 'current_user_role';
+    public const NAV_MENU = 'nav_menu';
+    public const PAGE_TYPE = 'page_type';
+    public const POST_TAXONOMY = 'post_taxonomy';
+    public const POST_CATEGORY = 'post_category';
+    public const BLOCK = 'block';
+    public const OPTIONS_PAGE = 'options_page';
+}
